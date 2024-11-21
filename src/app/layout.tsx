c86587701f810413899043514bab0bad63dc850a
doppelgangercvpr25 <doppelgangercvpr25@gmail.com>
2024-11-21 02:22:11 -0600

    changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,21 +7,21 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "MegaScenes Web Viewer",
-	description: "A web viewer for 3d reconstructions",
+    title: "Doppelganger Viewer",
+    description: "A web viewer for 3d reconstructions",
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-			<GoogleAnalytics
-				gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!}
-			/>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+            <GoogleAnalytics
+                gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!}
+            />
+        </html>
+    );
 }
